5e56dfe34010ad4d691279609656cdc128b8531e
Amaimersion <amatsukine@gmail.com>
2018-07-19 16:13:47 +0300

    Add solutions for Most Wanted Letter

---
+++ solutions/Home/The Most Wanted Letter/1.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import string
+
+def checkio(text):
+    text = text.lower()
+    data = [(i, text.count(i)) for i in string.ascii_lowercase]
+    return max(data, key=lambda x: x[1])[0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
