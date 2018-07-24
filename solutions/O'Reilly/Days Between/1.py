9abe29f70058ae7cc5f2843b5a86a4597f378c01
Amaimersion <amatsukine@gmail.com>
2018-07-24 18:16:09 +0300

    Add solution for Days Between

---
+++ solutions/O'Reilly/Days Between/1.py
@@ -0,0 +1,17 @@
+import datetime
+
+def days_diff(date1, date2):
+    """
+        Find absolute diff in days between dates
+    """
+    date1 = datetime.date(*date1)
+    date2 = datetime.date(*date2)
+    diff = date1 - date2
+
+    return abs(diff.days)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
